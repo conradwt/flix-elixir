8d0119782fc080249b73c4085420412385dfd37d
Conrad Taylor <conradwt@gmail.com>
2021-03-28 19:08:58 -0700

    Add flash message when signing in.

--- lib/flix_web/controllers/user_session_controller.ex
+++ lib/flix_web/controllers/user_session_controller.ex
@@ -12,7 +12,9 @@ defmodule FlixWeb.UserSessionController do
     %{"email" => email, "password" => password} = user_params
 
     if user = Accounts.get_user_by_email_and_password(email, password) do
-      UserAuth.log_in_user(conn, user, user_params)
+      conn
+      |> put_flash(:info, "Logged in successfully.")
+      |> UserAuth.log_in_user(user, user_params)
     else
       render(conn, "new.html", error_message: "Invalid email or password")
     end
