f53f3acec8b4b5ad01c67bb286337d642eee3a32
Conrad Taylor <conradwt@gmail.com>
2021-05-25 19:43:58 -0700

    More code cleanup.

--- lib/flix_web/graphql/resolvers/review_resolver.ex
+++ lib/flix_web/graphql/resolvers/review_resolver.ex
@@ -1,5 +1,8 @@
 defmodule FlixWeb.Graphql.Resolvers.ReviewResolver do
+
+  alias Flix.Catalogs
+
   def list_reviews(%Flix.Catalogs.Movie{} = movie, _args, _resolution) do
-    {:ok, Flix.Catalogs.list_reviews(movie)}
+    {:ok, Catalogs.list_reviews(movie)}
   end
 end
