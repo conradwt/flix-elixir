3f421242b741f07a8538e647cc90238fec9893f7
Conrad Taylor <conradwt@gmail.com>
2021-05-30 15:27:51 -0700

    Configure the cors_plug package.

--- lib/flix_web/endpoint.ex
+++ lib/flix_web/endpoint.ex
@@ -60,5 +60,8 @@ defmodule FlixWeb.Endpoint do
   plug(Plug.MethodOverride)
   plug(Plug.Head)
   plug(Plug.Session, @session_options)
+
+  plug CORSPlug, origin: ["*"]
+
   plug(FlixWeb.Router)
 end
