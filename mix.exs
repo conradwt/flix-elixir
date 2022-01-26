fb15eaa98cb71abb0f9196b212e3002dc0e29a91
Conrad Taylor <conradwt@gmail.com>
2022-01-25 21:22:27 -0800

    Upgrade software requirements.

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Flix.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.2"},
+      {:phoenix, "~> 1.6.6"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.7.1"},
       {:postgrex, "~> 0.15.9"},
