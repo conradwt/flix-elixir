c460ae3d2dd427ff106884d73a6340499fd3dbd1
Conrad Taylor <conradwt@gmail.com>
2022-01-31 22:05:24 -0800

    Add iex configuration file. (#88)

---
+++ .iex.exs
@@ -0,0 +1,13 @@
+alias Flix.Accounts
+alias Flix.Accounts.User
+
+alias Flix.Catalogs
+alias Flix.Catalogs.Characterization
+alias Flix.Catalogs.Favorite
+alias Flix.Catalogs.Genre
+alias Flix.Catalogs.Movie
+alias Flix.Catalogs.Review
+
+alias Flix.Repo
+
+import Ecto.Query
