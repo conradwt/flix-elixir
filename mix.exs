576c58bab5b9eb9b52267e0e0f4f4c822d6925cf
Conrad Taylor <conradwt@gmail.com>
2022-02-28 20:06:04 -0800

    Add deployment alias.

--- mix.exs
+++ mix.exs
@@ -80,7 +80,8 @@ defmodule Flix.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
   end
 end
