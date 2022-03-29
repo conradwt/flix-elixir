7313e8b8702af3fbdbfc11494927bf1bfb35707f
Conrad Taylor <conradwt@gmail.com>
2022-03-28 23:28:01 -0700

    WIP: migrate from Webpack to Esbuild.

--- config/config.exs
+++ config/config.exs
@@ -30,6 +30,24 @@ config :flix, Flix.Mailer, adapter: Swoosh.Adapters.Local
 # Swoosh API client is needed for adapters other than SMTP.
 config :swoosh, :api_client, false
 
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.14.0",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configure dart_sass (the version is required)
+config :dart_sass,
+  version: "1.49.9",
+  default: [
+    args: ~w(css/app.scss: ../priv/static/assets/app.css),
+    cd: Path.expand("../assets", __DIR__)
+  ]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
