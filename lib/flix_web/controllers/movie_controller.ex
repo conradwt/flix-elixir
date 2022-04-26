62b45d196cad42cb3f2528e1ddf3136b7a1475b4
Conrad Taylor <conradwt@gmail.com>
2022-04-26 14:24:03 -0700

    Fix Ecto reset.

--- lib/flix_web/controllers/movie_controller.ex
+++ lib/flix_web/controllers/movie_controller.ex
@@ -31,12 +31,12 @@ defmodule FlixWeb.MovieController do
 
   def create(conn, %{"movie" => movie_params}) do
     case Catalogs.create_movie(movie_params) do
-      {:ok, %{movie_info: movie_info, movie_poster: _movie_poster}} ->
+      {:ok, %{movie: movie_info, movie_poster: _movie_poster}} ->
         conn
         |> put_flash(:notice, "Movie created successfully.")
         |> redirect(to: Routes.movie_path(conn, :show, movie_info))
 
-      {:error, :movie_info, changeset, _changes_so_far} ->
+      {:error, :movie, changeset, _changes_so_far} ->
         conn
         |> put_flash(:error, "There was a problem creating the movie.  Please try again.")
         |> render("new.html",
