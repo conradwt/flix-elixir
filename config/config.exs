5f3066ddc613b445fa19e1a05569a56a4f7c307e
Conrad Taylor <conradwt@gmail.com>
2022-04-26 14:07:49 -0700

    Fix Credo configuration. (#121)

--- config/config.exs
+++ config/config.exs
@@ -49,14 +49,14 @@ config :flix, Flix.Mailer,
   api_key: System.get_env("SENDGRID_API_KEY")
 
 # Configure mix_test_watch
-# if Mix.env == :dev do
-#   config :mix_test_watch,
-#     clear: true,
-#     tasks: [
-#       "test",
-#       "credo",
-#     ]
-# end
+if Mix.env == :dev do
+  config :mix_test_watch,
+    clear: true,
+    tasks: [
+      "test",
+      "credo"
+    ]
+end
 
 # Configure Absinthe SDL/JSON code generation.
 config :absinthe,
