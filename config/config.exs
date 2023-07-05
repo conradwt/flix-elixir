6b4fb78c20ddd9cd1161eae645bb3101bd587c86
Conrad Taylor <conradwt@gmail.com>
2023-07-04 22:56:28 -0700

    Update to Elixir 1.15.2 and refresh Hex packages.

--- config/config.exs
+++ config/config.exs
@@ -32,7 +32,7 @@ config :swoosh, :api_client, false
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.17.19",
+  version: "0.18.11",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
